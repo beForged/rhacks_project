bdcd1c108565bc0994246e2cfd4bf360096c083c
Ivan Quiles <ivan98q@gmail.com>
2017-04-16 08:03:56 -0400

    Add files via upload

---
+++ custom_TrackedObject.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class custom_TrackedObject : SteamVR_TrackedObject
+{
+    VRControllerState_t state;
+    VRControllerState_t prevState;
+
+    public grabObject activeBeamInterceptObj;
+
+    float beamDist;
+
+    CVRSystem vrSystem;
+
+    public Ray deviceRay;
+
+    public Vector3 currPosition;
+    public Vector3 currRightVec;
+    public Vector3 currUpVec;
+    public Vector3 currForwardVec;
+    public Quaternion currRotation;
+    public float currRayAngle;
+    
+    void Start()
+    {
+        currRayAngle = 0;
+        vrSystem = OpenVR.System;
+    }
+
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(deviceRay.origin, deviceRay.direction, out hitInfo, Mathf.Infinity))
+        {
+            if (hitInfo.collider.gameObject.GetComponent<grabObject>())
+            {
+                activeBeamInterceptObj = hitInfo.collider.GetComponent<grabObject>();
+                beamDist = hitInfo.distance;
+            }
+            else
+            {
+                activeBeamInterceptObj = null;
+            }
+           
+        }
+        else
+        {
+            activeBeamInterceptObj = null;
+        }
+
+        currPosition = transform.position;
+        currRightVec = transform.right;
+        currUpVec = transform.up;
+        currForwardVec = transform.forward;
+        currRotation = transform.rotation;
+        deviceRay.origin = currPosition;
+
+        //Quaternion rayRotation = Quaternion.AngleAxis(currRayAngle, currRightVec);
+
+        deviceRay.direction = currForwardVec;
+
+        handleStateChanges();
+
+
+    }
+
+    void handleStateChanges()
+    {
+        bool stateIsValid = vrSystem.GetControllerState((uint) index, ref state,
+            (uint) System.Runtime.InteropServices.Marshal.SizeOf(typeof(VRControllerState_t)));
+
+
+        if (!stateIsValid) Debug.Log("Invalid State for Idx: " + index);
+
+        if (stateIsValid && state.GetHashCode() != prevState.GetHashCode())
+        {
+            if ((state.ulButtonPressed & SteamVR_Controller.ButtonMask.Trigger) != 0 &&
+                (prevState.ulButtonPressed & SteamVR_Controller.ButtonMask.Trigger) == 0)
+            {
+                if (activeBeamInterceptObj == null)
+                {
+                    return;
+                }
+                activeBeamInterceptObj.grab(this);
+
+            }
+            else if ((state.ulButtonPressed & SteamVR_Controller.ButtonMask.Trigger) == 0 &&
+                     (prevState.ulButtonPressed & SteamVR_Controller.ButtonMask.Trigger) != 0)
+            {
+                if (activeBeamInterceptObj == null)
+                {
+                    return;
+                }
+                activeBeamInterceptObj.release(this);
+                activeBeamInterceptObj = null;
+            }
+
+            prevState = state;
+        }
+    }
+}
---
+++ grabObject.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class grabObject : MonoBehaviour {
+    custom_TrackedObject activeGrabObject;
+
+    Quaternion initialRotation;
+    Vector3 inititalOffset;
+
+    bool activeScale = false;
+    bool activeMove = false;
+
+    Vector3 prevpos;
+
+ 
+    void Start()
+    {
+        activeGrabObject = null;
+        gameObject.AddComponent<Rigidbody>();
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void Update()
+    {
+        updateMove();
+    }
+
+    void updateMove()
+    {
+        if (activeMove)
+        {
+            
+            gameObject.transform.rotation = activeGrabObject.currRotation * initialRotation;
+
+            
+            gameObject.transform.position = activeGrabObject.deviceRay.origin +
+                    inititalOffset.x * activeGrabObject.currUpVec +
+                    inititalOffset.y * activeGrabObject.currRightVec +
+                    inititalOffset.z * activeGrabObject.currForwardVec;
+            prevpos = transform.position;
+
+        }
+    }
+
+
+    public void grab(custom_TrackedObject controller)
+    {
+        activeGrabObject = controller;
+
+        if (activeGrabObject == null)
+        {
+            activeMove = false;
+        }
+        else
+        {
+            initialRotation = Quaternion.Inverse(activeGrabObject.currRotation) * gameObject.transform.rotation;
+            Vector3 tmpVec = gameObject.transform.position - activeGrabObject.currPosition;
+
+            inititalOffset.Set(
+                Vector3.Dot(activeGrabObject.currUpVec, tmpVec),
+                Vector3.Dot(activeGrabObject.currRightVec, tmpVec),
+                Vector3.Dot(activeGrabObject.currForwardVec, tmpVec)
+                );
+            activeMove = true;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    public void release(custom_TrackedObject controller)
+    {
+       
+
+        activeGrabObject = null;
+        activeMove = false;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+        //gameObject.GetComponent<Rigidbody>().AddForce(gameObject.GetComponent<VelocityEstimator>().GetVelocityEstimate());
+    }
+
+
+
+
+}
